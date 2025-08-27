0ad760fb7c586bda4f28abbbcb8b12a58ad5faac
Aswath64 <144975456+Aswath64@users.noreply.github.com>
2025-08-27 17:43:35 +0530

    Update LoanApplicationController.java

--- src/main/java/com/examly/springapp/controller/LoanApplicationController.java
+++ src/main/java/com/examly/springapp/controller/LoanApplicationController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/applications")
-@CrossOrigin(origins = "https://expense-reimbursement-system-fronte.vercel.app/login")
+@CrossOrigin(origins = "https://expense-reimbursement-system-fronte.vercel.app")
 public class LoanApplicationController {
 
     @Autowired
